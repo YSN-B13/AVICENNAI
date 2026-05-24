9f5fa04d126d6ca32f729d8ec435ca2f18028458
YASSINE B13 <yagamiilight1986@gmail.com>
2026-05-24 14:28:30 +0100

    feat: upload app.py

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from avicennai!")
-
-
-if __name__ == "__main__":
-    main()
